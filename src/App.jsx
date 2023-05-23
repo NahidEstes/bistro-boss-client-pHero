b5b57dcff546ec44c5b4d7dd0e6a4288c2b2f188
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-05-23 21:47:06 +0600

    added a design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-10 h-10 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              Organically grow
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A slice of heaven. O for awesome, this chocka full cuzzie is as
+              rip-off as a cracker. Meanwhile, in behind the bicycle shed,
+              Hercules Morse, as big as a horse and Mrs Falani were up to no
+              good with a bunch of crook pikelets.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-10 h-10 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              A slice of heaven
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Disrupt inspire and think tank, social entrepreneur but
+              preliminary thinking think tank compelling. Inspiring, invest
+              synergy capacity building, white paper; silo, unprecedented
+              challenge B-corp problem-solvers.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
