49d1e5de1935aae17323538e6c76e47dc4295355
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-05-24 21:32:07 +0600

    add banner on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Banner from "./Pages/Home/Banner/Banner";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
-          <div className="mr-4">
-            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
-              <svg
-                className="w-10 h-10 text-deep-purple-accent-400"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
-              >
-                <polygon
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
-                />
-              </svg>
-            </div>
-          </div>
-          <div>
-            <h6 className="mb-3 text-xl font-bold leading-5">
-              Organically grow
-            </h6>
-            <p className="mb-3 text-sm text-gray-900">
-              A slice of heaven. O for awesome, this chocka full cuzzie is as
-              rip-off as a cracker. Meanwhile, in behind the bicycle shed,
-              Hercules Morse, as big as a horse and Mrs Falani were up to no
-              good with a bunch of crook pikelets.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
-          <div className="mr-4">
-            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
-              <svg
-                className="w-10 h-10 text-deep-purple-accent-400"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
-              >
-                <polygon
-                  strokeWidth="3"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
-                />
-              </svg>
-            </div>
-          </div>
-          <div>
-            <h6 className="mb-3 text-xl font-bold leading-5">
-              A slice of heaven
-            </h6>
-            <p className="mb-3 text-sm text-gray-900">
-              Disrupt inspire and think tank, social entrepreneur but
-              preliminary thinking think tank compelling. Inspiring, invest
-              synergy capacity building, white paper; silo, unprecedented
-              challenge B-corp problem-solvers.
-            </p>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
-      </div>
+      <Banner />
     </>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../../assets/home/01.jpg";
+import img2 from "../../../assets/home/02.jpg";
+import img3 from "../../../assets/home/03.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <Carousel>
+        <div>
+          <img src={img1} />
+        </div>
+        <div>
+          <img src={img2} />
+        </div>
+        <div>
+          <img src={img3} />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
