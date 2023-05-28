0468481feeefd37d38c5bc457de1d119c2b22f2b
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-05-28 08:46:33 +0600

    beatifully designed cover menu section with parallax node package

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -7,6 +10,14 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Our Menu</title>
       </Helmet>
+      <Cover img={menuImg} title="our menu" />
+      <PopularMenu />
+
+      <Cover img={menuImg} title="our menu" />
+      <PopularMenu />
+
+      <Cover img={menuImg} title="our menu" />
+      <PopularMenu />
     </div>
   );
 };
---
+++ src/Shared/Cover/Cover.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+
+const Cover = ({ img, title }) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={img}
+      bgImageAlt="the dog"
+      strength={-200}
+    >
+      <div className="hero h-[700px]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+            <p className="mb-5"></p>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+
+export default Cover;
