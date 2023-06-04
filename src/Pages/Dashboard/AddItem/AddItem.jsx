c29a3b0483c307c1adddcebd225a55d10daa6d50
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-06-04 14:54:13 +0600

    send image to the imgbb api

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -2,10 +2,23 @@ import Swal from "sweetalert2";
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 
+const image_hosting_token = import.meta.env.VITE_Image_Upload_token;
+
 const AddItem = () => {
   const { register, handleSubmit, reset } = useForm();
+  const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${image_hosting_token}`;
+  console.log(image_hosting_token);
   const onSubmit = (data) => {
-    console.log(data);
+    const formData = new FormData();
+    formData.append("image", data.image[0]);
+    fetch(img_hosting_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgResponse) => {
+        console.log(imgResponse);
+      });
   };
   return (
     <div className="w-full px-10">
